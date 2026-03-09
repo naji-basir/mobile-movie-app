34c140b7d41bad59ecc4cec73e6a969db2bb1f74
Basir Naji <basirnaji007@gmail.com>
2026-03-10 00:49:29 +0430

    routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import "./globals.css";
 
@@ -6,15 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-5xl text-dark-200 font-bold">Basir Naji</Text>
-      <Link href="/onboarding">onboarding</Link>
-      <Link
-        href={{
-          pathname: "/movie/[id]",
-          params: { id: "avengers" },
-        }}
-      >
-        Avengers Movie
-      </Link>
     </View>
   );
 }
