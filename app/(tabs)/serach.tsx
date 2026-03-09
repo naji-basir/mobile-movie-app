558cf117e380e5ec9d0cb165c839f1b33478acf6
Basir Naji <basirnaji007@gmail.com>
2026-03-10 01:06:36 +0430

    tabs routes

---
+++ app/(tabs)/serach.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const serach = () => {
+  return (
+    <View>
+      <Text>serach</Text>
+    </View>
+  );
+};
+
+export default serach;
+
+const styles = StyleSheet.create({});
